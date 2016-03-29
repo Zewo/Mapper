6cc8d7a1550e3b19dba84f4c33806c13ead065b2
Oleg Dreyman <dreymonde@me.com>
2016-03-29 13:04:42 +0300

    Package rename to InterchangeDataMapper

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Topo",
+    name: "InterchangeDataMapper",
     dependencies: [
     	.Package(url: "https://github.com/Zewo/InterchangeData.git", majorVersion: 0, minor: 4)
     ]

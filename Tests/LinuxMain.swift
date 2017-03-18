aacb9cb2001a87bfaf44aa68f9ab0d98a899d009
Oleg Dreyman <dreymonde@me.com>
2017-03-18 18:18:05 +0200

    Fix Linux tests

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,6 +1,55 @@
 import XCTest
 @testable import MapperTests
 
+extension InMapperTests {
+    static var allTests = [
+        ("testPrimitiveMapping", testPrimitiveMapping),
+        ("testBasicNesting", testBasicNesting),
+        ("testFailNoValue", testFailNoValue),
+        ("testFailWrongType", testFailWrongType),
+        ("testFailRepresentAsArray", testFailRepresentAsArray),
+        ("testArrayOfMappables", testArrayOfMappables),
+        ("testEnumMapping", testEnumMapping),
+        ("testEnumArrayMapping", testEnumArrayMapping),
+        ("testEnumFail", testEnumFail),
+        ("testBasicMappingWithContext", testBasicMappingWithContext),
+        ("testContextInference", testContextInference),
+        ("testArrayMappingWithContext", testArrayMappingWithContext),
+        ("testUsingContext", testUsingContext),
+        ("testDeep", testDeep),
+        ("testFlatArray", testFlatArray),
+        ("testAdvancedEnum", testAdvancedEnum),
+        ("testExternalMappable", testExternalMappable),
+        ("testDateMapping", testDateMapping),
+        ]
+}
+extension OutMapperTests {
+    static var allTests = [
+        ("testPrimitiveTypesMapping", testPrimitiveTypesMapping),
+        ("testBasicNesting", testBasicNesting),
+        ("testArrayOfMappables", testArrayOfMappables),
+        ("testEnumMappng", testEnumMappng),
+        ("testEnumArrayMapping", testEnumArrayMapping),
+        ("testBasicMappingWithContext", testBasicMappingWithContext),
+        ("testContextInference", testContextInference),
+        ("testArrayMappingWithContext", testArrayMappingWithContext),
+        ("testUsingContext", testUsingContext),
+        ("testExternalMappable", testExternalMappable),
+        ("testDateMapping", testDateMapping),
+        ("testWithOptions", testWithOptions),
+        ("testWithOptionsNest", testWithOptionsNest),
+        ]
+}
+extension PerformanceTests {
+    static var allTests = [
+        ("testPerformancePrim", testPerformancePrim),
+        ("testPerformanceUnsafe", testPerformanceUnsafe),
+        ("testPerformanceDirect", testPerformanceDirect),
+        ]
+}
+
 XCTMain([
-     testCase(MapperTests.allTests),
+    testCase(InMapperTests.allTests),
+    testCase(OutMapperTests.allTests),
+    testCase(PerformanceTests.allTests),
 ])
